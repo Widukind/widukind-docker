3b1e6c6bdae094a17b4e25cd4db99d66355ca676
Stéphane RAULT <stephane.rault@radicalspam.org>
2016-01-07 11:42:16 +0100

    optimize redis

--- redis-docker/docker-entrypoint.sh
+++ redis-docker/docker-entrypoint.sh
@@ -3,7 +3,8 @@ set -e
 
 sysctl -w net.core.somaxconn=65535
 sysctl -w vm.overcommit_memory=1
-echo never > /sys/kernel/mm/transparent_hugepage/enabled
+
+[ -f /sys/kernel/mm/transparent_hugepage/enabled ] && echo never > /sys/kernel/mm/transparent_hugepage/enabled
 
 if [ "$1" = 'redis-server' ]; then
         chown -R redis .
